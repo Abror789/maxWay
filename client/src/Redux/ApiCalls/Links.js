190b2bffcb9f97d0971c1e8b9d3d9d1746cec58c
abror_muzaffarovich <83820702+Abror789@users.noreply.github.com>
2022-12-25 00:20:46 +0500

    Delete client/src/Redux directory

--- client/src/Redux/ApiCalls/Links.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-import { baseURl } from "../../components/Variables"
-
-export const getLinks=async()=>{
-    const {data}=await axios.get(`${baseURl}links`)
-    return data
-}
